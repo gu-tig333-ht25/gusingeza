203cc4b15cd3f97dd7b1a64c12c85d6ee29947a2
Aizerll <zingerstrom@outlook.com>
2025-09-26 16:40:02 +0200

    Vecka-3 klar

--- lib/mystate.dart
+++ lib/mystate.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'main.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class MyState extends ChangeNotifier {
-  final List<Task> _tasks = [
-    Task("Clean room"),
-    Task("Do dishes"),
-    Task("Make food"),
-  ];
+  final String apiKey = "9a2cb6ce-ee5c-4cbe-abb4-f7c829c242ca";
+  List<Task> _tasks = [];
 
   TodoFilter _filter = TodoFilter.all; // aktuell filter
   TodoFilter get filter => _filter;
+
+  MyState() {
+    fetchTasks();
+  }
 
   List<Task> get tasks {
     switch (_filter) {
@@ -22,23 +25,44 @@ class MyState extends ChangeNotifier {
     }
   }
 
+  Future<void> fetchTasks() async {
+    final response = await http.get(Uri.parse("https://todoapp-api.apps.k8s.gu.se/todos?key=$apiKey"),
+    );
+    final List data = jsonDecode(response.body);
+    _tasks = data.map((json) => Task.fromJson(json)).toList().cast<Task>();
+    notifyListeners();
+  }
+
+  Future<void> addTask(String title) async {
+    await http.post(
+      Uri.parse("https://todoapp-api.apps.k8s.gu.se/todos?key=$apiKey"),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({"title": title, "done": false}),
+    );
+    await fetchTasks();
+  }
+
+  Future<void> removeTask(Task task) async {
+    await http.delete(
+      Uri.parse("https://todoapp-api.apps.k8s.gu.se/todos/${task.id}?key=$apiKey"),
+    );
+    await fetchTasks();
+  } 
+
+  Future<void> toggleTask(Task task) async {
+    final updated = Task(task.title, isDone: !task.isDone, id: task.id);
+
+    await http.put(
+      Uri.parse("https://todoapp-api.apps.k8s.gu.se/todos/${task.id}?key=$apiKey"),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode(updated.toJson()),
+    );
+    await fetchTasks();
+  }
+
   void setFilter(TodoFilter filter) {
     _filter = filter;
     notifyListeners();
   }
-
-  void addTask(String task) {
-    _tasks.add(Task(task));
-    notifyListeners();
-  }
-
-  void removeTask(Task task) {
-    _tasks.remove(task);
-    notifyListeners();
-  }
-
-  void toggleTask(Task task) {
-    task.isDone = !task.isDone;
-    notifyListeners();
-  }
+  
 }
